6adacf49faedf67abfc76ed74bf9cbe80a7956dd
zaidkhan1123 <zk117854@gmail.com>
2022-11-01 00:04:42 +0500

    ee

--- src/Todoprc.jsx
+++ src/Todoprc.jsx
@@ -22,12 +22,13 @@ function Todo(props) {
    <div className="container ">
     <div className="row  ">
       <div className="col-md-3 offset-md-4 main mt-5   ">
-  <span className='fs-5'>Name</span> : <input className='mt-3 ms-3' placeholder='Name' type="text" value={input} onChange={(e)=>{setinput(e.target.value)}}/> {input.length == '' ? <div className='error'>Input is Empty</div> : "" }
+        <h1 className='text-center mt-5'>Login Form</h1>
+  <span className='fs-5 fw-bold '>Name</span> : <input className='mt-3 ms-3' placeholder='Name' type="text" value={input} onChange={(e)=>{setinput(e.target.value)}}/> {input.length == '' ? <div className='error'>Input is Empty</div> : "" }
   <div>
-  <span className='fs-5'>Email</span> : <input className='mt-3 ms-4'  placeholder='Email' type="text" value={email} onChange={(e)=>{setemail(e.target.value)}}/> {email.length == '' ? <div className='error'>Input is Empty</div> : "" }
+  <span className='fs-5 fw-bold'>Email</span> : <input className='mt-3 ms-4'  placeholder='Email' type="text" value={email} onChange={(e)=>{setemail(e.target.value)}}/> {email.length == '' ? <div className='error'>Input is Empty</div> : "" }
   </div>
   <div>
-<span className='fs-5'>Password</span>  <input className='mt-3'  placeholder='Password' type="password" value={password} onChange={(e)=>{setpassword(e.target.value)}}/> {password.length == '' ? <div className='error'>Input is Empty</div> : "" }
+<span className='fs-5 fw-bold'>Password</span>  <input className='mt-3'  placeholder='Password' type="password" value={password} onChange={(e)=>{setpassword(e.target.value)}}/> {password.length == '' ? <div className='error'>Input is Empty</div> : "" }
   </div>
   <div className="col text-center">
   <button className='btn btn-primary mt-5' onClick={Submit}>Submit</button>
