9773c0040c55202bb656cb1a21e365652128e669
zaidkhan1123 <zk117854@gmail.com>
2022-10-31 17:00:26 +0500

    new

---
+++ src/Todoprc.jsx
@@ -0,0 +1,48 @@
+import React,{useState} from 'react'
+
+function Todo(props) {
+
+  const [input,setinput] = useState("")
+  const [email,setemail] = useState("")
+  const [password,setpassword] = useState("")
+
+
+ function Submit(){
+  if(input == ''){
+    alert("your  input is Empty")
+  }else if(email == ''){
+    alert("Your  input is Empty")
+   }else if(password == ""){
+    alert("your  input is Empty")
+   }
+ }
+
+  return <>
+ 
+   <div className="container ">
+    <div className="row  ">
+      <div className="col-md-3 offset-md-4 main mt-5   ">
+  <span className='fs-5'>Name</span> : <input className='mt-3 ms-3' placeholder='Name' type="text" value={input} onChange={(e)=>{setinput(e.target.value)}}/> {input.length == '' ? <div className='error'>Input is Empty</div> : "" }
+  <div>
+  <span className='fs-5'>Email</span> : <input className='mt-3 ms-4'  placeholder='Email' type="text" value={email} onChange={(e)=>{setemail(e.target.value)}}/> {email.length == '' ? <div className='error'>Input is Empty</div> : "" }
+  </div>
+  <div>
+<span className='fs-5'>Password</span>  <input className='mt-3'  placeholder='Password' type="password" value={password} onChange={(e)=>{setpassword(e.target.value)}}/> {password.length == '' ? <div className='error'>Input is Empty</div> : "" }
+  </div>
+  <div className="col text-center">
+  <button className='btn btn-primary mt-5' onClick={Submit}>Submit</button>
+  </div>
+</div>
+  </div>
+
+  </div>
+
+      
+
+  </>
+   
+  
+   
+}
+
+export default Todo
